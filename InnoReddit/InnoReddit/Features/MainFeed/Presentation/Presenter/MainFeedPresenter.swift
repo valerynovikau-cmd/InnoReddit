1d1b4c8618515b57406cc066e90cf9000c097793
ValeryNovikau <valery.novikau@innowise.com>
2025-11-12 17:54:36 +0300

    [ADDED] Added posts array fields and post retrieval method to main feed presenter

--- InnoReddit/InnoReddit/Features/MainFeed/Presentation/Presenter/MainFeedPresenter.swift
+++ InnoReddit/InnoReddit/Features/MainFeed/Presentation/Presenter/MainFeedPresenter.swift
@@ -5,10 +5,33 @@
 //  Created by Валерий Новиков on 11.11.25.
 //
 
+import Factory
+
 final class MainFeedPresenter {
     weak var input: MainFeedViewProtocol?
+    @Injected(\.mainFeedNetworkService) private var networkService: MainFeedNetworkServiceProtocol
+    @Injected(\.mainFeedModelMapper) private var modelMapper: MainFeedModelMapperProtocol
+    
+    private(set) var bestPosts: [Post] = []
+    private(set) var hotPosts: [Post] = []
+    private(set) var newPosts: [Post] = []
+    private(set) var topPosts: [Post] = []
+    private(set) var risingPosts: [Post] = []
 }
 
 extension MainFeedPresenter: MainFeedPresenterProtocol {
-    
+    func preformHotPostsRetrieval() {
+        Task {
+            do {
+                let posts = try await self.networkService.getHotPosts()
+                let mappedPosts = self.modelMapper.map(from: posts)
+                self.hotPosts = mappedPosts
+                self.input?.onHotPostsUpdated()
+            } catch {
+                //Это не bad practice если тебе было весело
+                print(error)
+                fatalError()
+            }
+        }
+    }
 }
--- InnoReddit/InnoReddit/Features/MainFeed/Presentation/Presenter/MainFeedPresenterProtocol.swift
+++ InnoReddit/InnoReddit/Features/MainFeed/Presentation/Presenter/MainFeedPresenterProtocol.swift
@@ -7,4 +7,10 @@
 
 protocol MainFeedPresenterProtocol: AnyObject {
     var input: MainFeedViewProtocol? { get set }
+    var bestPosts: [Post] { get }
+    var hotPosts: [Post] { get }
+    var newPosts: [Post] { get }
+    var topPosts: [Post] { get }
+    var risingPosts: [Post] { get }
+    func preformHotPostsRetrieval()
 }
